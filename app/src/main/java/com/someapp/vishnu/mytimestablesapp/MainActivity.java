4e7f083b1c65df84519cb183b42e0a2bb37bfa89
Vishnu Adda <addavishnu@gmail.com>
2018-05-28 20:36:07 -0700

    Completed the whole app basically. Updated the ListView to match the progress of the SeekBar. The SeekBar allows the user to find the table from 1 to 20.

--- app/src/main/java/com/someapp/vishnu/mytimestablesapp/MainActivity.java
+++ app/src/main/java/com/someapp/vishnu/mytimestablesapp/MainActivity.java
@@ -2,12 +2,76 @@ package com.someapp.vishnu.mytimestablesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView timesView;
+
+    ArrayList<Integer> timesList;
+
+    ArrayAdapter<Integer> timesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timesView = findViewById(R.id.timeListView);
+
+        timesList = new ArrayList<Integer>();
+
+        SeekBar numberControl = findViewById(R.id.seekBar);
+
+        numberControl.setMax(19);
+        numberControl.setProgress(5);
+
+        int size = 50;
+
+        for (int i = 1; i <= size; i++) {
+
+            timesList.add((i-1), numberControl.getProgress()*i);
+
+        }
+
+        timesAdapter = new ArrayAdapter<Integer>(this,
+                android.R.layout.simple_list_item_1, timesList);
+
+        timesView.setAdapter(timesAdapter);
+
+        numberControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int size = 50;
+
+                for (int i = 1; i <= size; i++) {
+
+                    timesList.set((i-1), (progress+1)*(i));
+
+                }
+
+                timesView.setAdapter(timesAdapter);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
